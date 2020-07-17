1ce9d7f408d4963edc9e7e166588e6a5e555c8cc
Rohun Vora <emailrohun@gmail.com>
2020-07-17 14:08:39 -0700

    [Add 5 Variables]

---
+++ app.js
@@ -0,0 +1,20 @@
+let stringy = "Rohun";
+console.log(stringy);
+
+let numero = 3;
+console.log(numero);
+
+let arrayz = [1, 2, 3, 4, 5, 6, 7]
+console.log(arrayz);
+
+let thisWorks = true;
+console.log(thisWorks);
+
+let car = {
+  name: "Toyota Camry",
+  license: 489302,
+  wheels: [1, 2, 3, 4],
+  carWorks: true
+}
+
+console.log(car);
