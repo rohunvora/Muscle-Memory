1c4ac6a7ed12896b2c064f7932db78a2fa70d013
Rohun Vora <emailrohun@gmail.com>
2020-07-17 16:10:28 -0700

    [add container 7]

--- app.js
+++ app.js
@@ -77,10 +77,71 @@ function funNine (array) {
 
 funNine(arrayz)
 
-function funTen () {
-  console.log(The end of the function sprint)
-}
 
-funTen();
+const sectionOne = document.querySelector('#section-one');
+console.log(sectionOne)
+
+const sectionTwo = document.querySelector('#section-two');
+console.log(sectionTwo)
+
+const sectionThree = document.querySelector('#section-three');
+console.log(sectionThree);
+
+const sectionFour = document.querySelector('#section-four');
+console.log(sectionFour);
+
+const sectionFive = document.querySelector('#section-five');
+console.log(sectionFive);
 
 
+sectionOne.textContent = "Lakers";
+sectionTwo.textContent = "Warriors";
+sectionThree.textContent = "Celtics";
+sectionFour.textContent = "Nets";
+sectionFive.textContent = "Blazers";
+
+const paraOne = document.querySelector('.paragraph-one');
+paraOne.textContent = 'LeBron James';
+
+const paraTwo = document.querySelector('.paragraph-two');
+paraTwo.textContent = 'Stephen Curry';
+
+const paraThree = document.querySelector('.paragraph-three');
+paraThree.textContent = 'Jayson Tatum';
+
+const paraFour = document.querySelector('.paragraph-four');
+paraFour.textContent = 'Kyrie Irving';
+
+const paraFive = document.querySelector('.paragraph-five');
+paraFive.textContent = 'Damian Lillard';
+
+const containerSix = document.createElement('div');
+
+const sectionSix = document.createElement('h2');
+sectionSix.setAttribute('id', 'section-six');
+sectionSix.textContent = "Rockets";
+
+const paraSix = document.createElement('p');
+paraSix.classList.add('paragraph-six');
+paraSix.textContent = "James Harden";
+
+const body = document.querySelector('body');
+
+body.appendChild(containerSix);
+containerSix.appendChild(sectionSix);
+containerSix.appendChild(paraSix);
+
+
+const containerSeven = document.createElement('div');
+
+const sectionSeven = document.createElement('h2');
+sectionSeven.setAttribute('id', 'section-seven');
+sectionSeven.textContent = "Mavericks";
+
+const paraSeven = document.createElement('p');
+paraSeven.classList.add('paragraph-seven');
+paraSeven.textContent = "Luka Doncic";
+
+body.appendChild(containerSeven);
+containerSeven.appendChild(sectionSeven);
+containerSeven.appendChild(paraSeven);
